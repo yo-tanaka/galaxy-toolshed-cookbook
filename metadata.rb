7c54a6ad2c205790e54b823e581f44156ab382dd
yo-tanaka <yo-tanaka@riken.jp>
2014-10-24 18:10:16 +0900

    edit metadata

--- metadata.rb
+++ metadata.rb
@@ -7,6 +7,6 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
 #supports          centos-6.5
-%w{ python mercurial yum vim }.each do |cb|
+%w{ python mercurial yum }.each do |cb|
     depends  cb
 end
